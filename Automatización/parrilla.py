289f620c6b6988817b604833c79014a016d3de6b
aag224 <119348147+aag224@users.noreply.github.com>
2022-11-28 13:53:50 -0600

    Delete parrilla.py

--- Automatización/parrilla.py
+++
@@ -1,8 +0,0 @@
-#import matplotlib.pyplot as plt
-#from matplotlib.pyplot import *
-#import numpy as np
-#from numpy import *
-
-import pylab.instruments 
-from pylab.instruments import list_visa_instruments
-list_visa_instruments()
